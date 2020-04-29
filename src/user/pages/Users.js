da6284cc573fa22b5853f8049212ddc94167edc9
Alex8711 <liuzhendell@gmail.com>
2020-04-29 01:07:55 -0600

    second commit

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -4,9 +4,9 @@ const Users = () => {
   const Users = [
     {
       id: "u1",
-      name: "max",
+      name: "Max",
       image:
-        "https://www.pexels.com/photo/golden-gate-bridge-san-francisco-california-1141853/",
+        "https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
       places: 3,
     },
   ];
